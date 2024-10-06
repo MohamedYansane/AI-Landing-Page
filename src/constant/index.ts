d2733880cfb73b09bb0068e3d6ad19d837f13413
Mohamed Yansane <yansane472@gmail.com>
2024-10-06 03:27:12 +0000

    creating a navLink for my footer

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,8 +1,15 @@
 import { title } from "process";
 
 export const navLinks = [
-    {href: "#", title: "Features"},
-    {href: "#", title: "Developers"},
-    {href: "#", title: "Pricing"},
-    {href: "#", title: "ChangeLogs"},
-]
+  { href: "#", title: "Features" },
+  { href: "#", title: "Developers" },
+  { href: "#", title: "Pricing" },
+  { href: "#", title: "ChangeLogs" },
+];
+export const navFooter = [
+  { href: "#", title: "Features" },
+  { href: "#", title: "Developers" },
+  { href: "#", title: "Company" },
+  { href: "#", title: "Blog" },
+  { href: "#", title: "ChangeLog" },
+];
