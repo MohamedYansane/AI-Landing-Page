075fbd8ea5ec57f20377f37883699b3de8802eb2
Mohamed Yansane <yansane472@gmail.com>
2024-10-06 03:26:44 +0000

    adding CallToAction && Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { Testimonials } from "@/sections/Testimonials";
+import { CallToAction } from "@/sections/CallToAction";
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
       <LogoTicker />
       <Features />
       <Testimonials />
+      <CallToAction />
+      <Footer />
     </>
   );
 }
