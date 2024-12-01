ed9993a3de3051abef0b0c4e51e5150d6a1d8478
Mohamed Yansane <yansane472@gmail.com>
2024-12-01 15:28:28 +0000

    adding the iframewidget component inside the layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import IframeWidget from "@/components/iframe/iframe-widget";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={twMerge(inter.className, 'bg-black text-white antialiased')}>{children}</body>
-      
+      <body
+        className={twMerge(inter.className, "bg-black text-white antialiased")}
+      >
+        {children}
+        <IframeWidget />
+      </body>
     </html>
   );
 }
